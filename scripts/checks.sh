bfbaea5ee65dfdbea3f071d78cebdfa952989509
3ps1lon <famaly20@gmail.com>
2021-12-08 11:28:40 +0300

    test2

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -3,7 +3,7 @@
 set -e
 
 declare -r FILTER=-build/c++11,-runtime/references,\
--whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-legal/copyright
+-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-legal/copyright,-whitespace/operators,-whitespace/comma,-whitespace/semicolon
 
 
 find ./include/ ./scripts/ ./sources/ -name "*.cpp" -or -name "*.hpp" -or -name ".h" | xargs cpplint --filter=$FILTER
