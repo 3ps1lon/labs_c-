c19b36d8fd8eac9036702e00c7935a28b0524e44
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-21 17:55:49 +0700

    Update task_3.cpp

--- sources/task_3.cpp
+++ sources/task_3.cpp
@@ -4,11 +4,9 @@
 
 // Task 3.
 void swap_el(std::vector < int > & input) {
-
   for (int i = 0; i < 3; i++) {
     int buf = input[17 + i];
     input[17 + i] = input[i];
     input[i] = buf;
-
   }
 }
