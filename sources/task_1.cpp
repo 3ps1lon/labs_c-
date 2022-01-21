c17d4c9e3e3d331c7bc337387b88f3fd1644a2ce
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-21 17:44:02 +0700

    Update task_1.cpp

--- sources/task_1.cpp
+++ sources/task_1.cpp
@@ -1,27 +1,28 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <tasks.hpp>
+
 #include <cmath>
 
 
 // Task 1.
-void multi_2(std::vector<int>& input) {
-  for (int& i: input){
-    i *=2;
+void multi_2(std::vector < int > & input) {
+  for (int & i: input) {
+    i *= 2;
   }
 
 }
 
-void reduce_a(std::vector<int>& input, int a) {
-    for (int& i: input){
-      i -= abs(a);
-    }
+void reduce_a(std::vector < int > & input, int a) {
+  for (int & i: input) {
+    i -= abs(a);
+  }
 }
 
-void div_on_first(std::vector<int>& input) {
-    int k = 0;
-    k = input[0];
-    for(int& i: input){
-      i /= k;
-    }
+void div_on_first(std::vector < int > & input) {
+  int k = 0;
+  k = input[0];
+  for (int & i: input) {
+    i /= k;
+  }
 }
