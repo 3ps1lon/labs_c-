ccbf1f70934d35220a6f91b1f13bf17479033836
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-21 17:45:05 +0700

    Update task_4.cpp

--- sources/task_4.cpp
+++ sources/task_4.cpp
@@ -3,31 +3,31 @@
 #include <tasks.hpp>
 
 // Task 4.
-void remove_negative(std::vector<int>& input) {
+void remove_negative(std::vector < int > & input) {
   int k = 0;
-    for (int& i: input){
-      if (i<0){
-        input.erase(input.begin() +k);
-        break;
-      }
-      k++;
+  for (int & i: input) {
+    if (i < 0) {
+      input.erase(input.begin() + k);
+      break;
     }
+    k++;
+  }
 }
 
-void remove_last_even(std::vector<int>& input) {
-    int k = 0;
-    int d = 0;
-    int tr = 0;
-    for (int& i: input){
-      if (i%2==0){
-         d = k;
-         tr = 1;
-      }
-      k++;
+void remove_last_even(std::vector < int > & input) {
+  int k = 0;
+  int d = 0;
+  int tr = 0;
+  for (int & i: input) {
+    if (i % 2 == 0) {
+      d = k;
+      tr = 1;
     }
+    k++;
+  }
 
-    if (tr==1){
-      input.erase(input.begin() + d);
-    }
+  if (tr == 1) {
+    input.erase(input.begin() + d);
+  }
 
 }
