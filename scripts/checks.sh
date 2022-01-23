43c057ff894da2cd6318ff8b9e18b627ab2ecca2
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-24 02:51:49 +0700

    Update checks.sh

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -3,7 +3,7 @@
 set -e
 
 declare -r FILTER=-build/c++11,-runtime/references,\
--whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-runtime/string
+-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-runtime/string,-build/header_guard
 
 find ./include/ ./scripts/ ./sources/ -name "*.cpp" -or -name "*.hpp" -or -name ".h" | xargs cpplint --filter=$FILTER
 
