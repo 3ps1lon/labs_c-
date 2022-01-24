6efb5b0a9cb721b1211f2d87116fb02118e0edff
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-24 08:47:13 +0700

    Update Task_7.cpp

--- sources/Task_7.cpp
+++ sources/Task_7.cpp
@@ -8,18 +8,18 @@ std::vector<std::string> split(const std::string& str, char sep){
 
 
 
-  for (int i = 0;i < (static_cast<int> (str.length())); i++){
+  for (int i = 0; i < (static_cast<int> (str.length())); i++){
     if (str[i] == sep){
       k++;
     }
 
   }
-  if (str.length() == 0 || k == (int) str.length()){
+  if (str.length() == 0 || k == static_cast<int> (str.length())){
     res = {};
     return res;
   }
 
-  for (int i=0;i < (static_cast<int> (str.length())); i++){
+  for (int i = 0; i < (static_cast<int> (str.length())); i++){
     if (str[i] == sep){
       res.push_back(temp);
       i++;
