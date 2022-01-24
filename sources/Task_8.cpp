fa5f3c4b767cd70085b99fa538826f67e16a273b
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-24 08:44:34 +0700

    Update Task_8.cpp

--- sources/Task_8.cpp
+++ sources/Task_8.cpp
@@ -5,13 +5,11 @@ std::string join(const std::vector<std::string>& str, const std::string& sep){
   res = "";
 
   for (int i =0;i<(int)str.size()-1;i++){
-    if (str[str.size()] != str[i] ){
+    if (str[str.size()] != str[i]){
       res+=str[i];
       res+=sep;
     }
-
   }
   res += str[str.size()-1];
   return res;
-
 }
