a395c0cd46d2485a34061975773a1251520af35d
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-24 08:50:43 +0700

    Update Task_5.cpp

--- sources/Task_5.cpp
+++ sources/Task_5.cpp
@@ -1,11 +1,12 @@
 #include "../tests/lab05.hpp"
 
-bool remove_first_negative_element(const std::vector<int>& vec, int& removed_element){
+bool remove_first_negative_element(const std::vector<int>& vec,
+                                   int& removed_element){
   std::vector<int> res;
   res = vec;
-  removed_element =0;
-  for (int i= 0; i<(int)vec.size();i++){
-    if (res[i]<0){
+  removed_element = 0;
+  for (int i = 0; i < (static_cast<int> (vec.size())); i++){
+    if (res[i] < 0){
       removed_element = vec[i];
       res.erase(res.begin() +i);
       return true;
