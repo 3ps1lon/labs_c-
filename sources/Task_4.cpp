67812fd9c1487fd7a52d3ed7238bfc9570e60552
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-24 08:50:03 +0700

    Update Task_4.cpp

--- sources/Task_4.cpp
+++ sources/Task_4.cpp
@@ -1,8 +1,7 @@
 #include "../tests/lab05.hpp"
 
 void sort(std::vector<float>& vec){
-
-  for(int i = 0; i < (static_cast<int> (vec.size())); i++){
+  for (int i = 0; i < (static_cast<int> (vec.size())); i++){
     for (int j = i+1; j < (static_cast<int> (vec.size())); j++){
       if (vec[i] > vec[j]){
         int buf;
